a8d4465f972fbedbccbc914c3a80d0c25695106e
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2024-05-07 12:18:34 +0530

    updated from pull and pushing now

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    let scrolled = window.scrollY;
-    let parallax = document.querySelector('.parallax');
-    parallax.style.transform = 'translateY(' + scrolled * 0.3 + 'px)';
-});
